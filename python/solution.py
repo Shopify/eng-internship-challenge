c424c76db980083eb9aef5e1bd94907c28a7da21
EPICxFLIPPER <ryanfraser832@icloud.com>
2024-05-08 10:40:32 -0700

    Created Stubs

--- python/solution.py
+++ python/solution.py
@@ -1,1 +1,43 @@
-print("test")
+
+
+## The key "SUPERSPY", WHER I=J
+key = [["S","U","P","E","R"],
+       ["Y","A","B","C","D"],
+       ["F","G","H","I","K"],
+       ["L","M","N","O","Q"],
+       ["T","V","W","X","Z"]]
+
+##The encrypted Message
+encrypted = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+
+##Effects: Decrypts the Rectangle Rule, Returns a two letter string
+##first, second = the first and second letters to decrypt
+def rect(first,second):
+    ##STUB
+    return ""
+
+##Effects: Decrypts the Row Rule, Returns a two letter string
+##first, second = the first and second letters to decrypt
+def row(first,second):
+    ##STUB
+    return ""
+
+##Effects: Decrypts the Col Rule, Returns a two letter string
+##first, second = the first and second letters to decrypt
+def col(first,second):
+    ##STUB
+    return ""
+
+##Effects: Removes all instances of "X" from the string and returns it
+##s = the string to remove X from
+def removeX(s):
+    ##STUB
+    return ""
+
+
+##Effects: Decrypts the encrypted global with the global key
+##         Prints the decrptyed string
+def decrypt():
+    ##Stub
+    print("works")
+
