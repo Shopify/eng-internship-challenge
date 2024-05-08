6cf0ce7e92791b51737585e4d214a842b301c1f1
Tala <slemantala@example.com>
2024-05-08 15:17:59 -0400

    create diagraph with respective values

--- python/solution.py
+++ python/solution.py
@@ -1,6 +1,33 @@
+from collections import OrderedDict
+
 secret_string = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
 key = "SUPERSPY"
 plain_text = ""
+alphabet = OrderedDict()
 
 # alphabet without j
-alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+for i, letter in enumerate('abcdefghiklmnopqrstuvwxyz', start=1):
+    alphabet[letter] = i
+
+# create diagraph that will contain the letters
+diagraph = [["" for _ in range(5)] for _ in range(5)]
+key_index = 0
+
+for row in diagraph:
+    row_index = 0
+    while row_index < len(row):
+        if key_index < len(key):
+            if key[key_index].lower() in alphabet:
+                row[row_index] = key[key_index]
+                alphabet.pop(key[key_index].lower())
+                row_index += 1
+            key_index += 1
+        else:
+            first_value = next(iter(alphabet))
+            row[row_index] = first_value.upper()
+            alphabet.pop(first_value)
+            row_index += 1
+            
+#visualize diagraph
+for row in diagraph:
+    print(row)
