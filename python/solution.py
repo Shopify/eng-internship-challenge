86a0a08c7e13646418c9099c7c5085c384826957
joshua-larsen <64284030+joshua-larsen@users.noreply.github.com>
2024-05-08 16:35:10 -0600

    Add pseudo code

--- python/solution.py
+++ python/solution.py
@@ -11,11 +11,23 @@ def main():
     print(decrypted)
 
 def decrypt(n, m):
-    
 
-    # Your decrypted string must by entirely UPPER CASE, and not include spaces, the letter "X", or special characters. Ensure you meet all these conditions before outputting the result.
-    # Your application must output only the decrypted Playfair Cipher string.
-    # ie: BANANAS not The decrypted text is: BANANAS
+    # Define variable to store new str and a counter
+    message_decrypted = ''
+    counter = 0
+
+    # Initiate a while loop which continues until the entire message has been decrypted
+    while counter < len(message_decrypted)
+        
+        # Define variable to store message chunks of two chars
+        chunks = 
+
+        # Break ciphertext into chunks of two chars
+
+        # Compare the 
+        # Your decrypted string must by entirely UPPER CASE, and not include spaces, the letter "X", or special characters. Ensure you meet all these conditions before outputting the result.
+        # Your application must output only the decrypted Playfair Cipher string.
+        # ie: BANANAS not The decrypted text is: BANANAS
     
 
 if __name__ == '__main__':
