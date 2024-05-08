7e0c6294c95cd0a8a5e2294c92d02055ac170eae
Tala <slemantala@example.com>
2024-05-08 14:50:16 -0400

    initialize variables

---
+++ python/solution.py
@@ -0,0 +1,6 @@
+secret_string = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+key = "SUPERSPY"
+plain_text = ""
+
+# alphabet without j
+alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
