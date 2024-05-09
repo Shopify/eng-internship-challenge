838b977983d4f24c92983d99b8c18f5ed3efdb51
Kishor Pandya <kishorpandya17@gmail.com>
2024-05-08 20:43:31 -0400

    Completed implementation of remaning functions

--- python/solution.py
+++ python/solution.py
@@ -11,12 +11,31 @@ def get_cypher(key):
         key_matrix[i//5].append(complete_key[i])
     return key_matrix
 
+def decrypt_two_characters(two_characters, key_matrix):
+    first_character = find_index(key_matrix,two_characters[0])
+    second_character = find_index(key_matrix,two_characters[1])
+
+    if first_character[0] == second_character[0]:
+        return key_matrix[first_character[0]][first_character[1]-1 if first_character[1]!=0 else 4] + key_matrix[second_character[0]][second_character[1]-1 if second_character[1]!=0 else 4]
+    elif first_character[1] == second_character[1]:
+        return key_matrix[first_character[0]-1 if first_character[0]!=0 else 4][first_character[1]] + key_matrix[second_character[0]-1 if second_character[0]!=0 else 4][second_character[1]]
+    else:
+        return key_matrix[first_character[0]][second_character[1]] + key_matrix[second_character[0]][first_character[1]]
+
+def find_index(key_matrix, character):
+    for row in range(0,5):
+        for col in range(0,5):
+            if character == key_matrix[row][col]:
+                return (row, col)
+
 def decrypt_message(encrypted_message, key):
     key_matrix = get_cypher(key)
     if len(encrypted_message) % 2 != 0:
         encrypted_message += 'X'
-    
-    return key_matrix
+    decrypted_message = ''
+    for i in range(0, len(encrypted_message), 2):
+        decrypted_message += decrypt_two_characters(encrypted_message[i:i+2], key_matrix)
+    return decrypted_message.replace('X','')
 
 
 key = "SUPERSPY"
