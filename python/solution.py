5b0cf434e30fcbf5a0b4e888265730b27770019b
Porom <porom.kamal@gmail.com>
2024-05-08 20:08:35 -0400

    feat: Added first iteration of playfair matrix generator

--- python/solution.py
+++ python/solution.py
@@ -11,7 +11,36 @@ class PlayFairCipherDecoder:
     Returns:
       grid (list(list(str))): The Playfair Grid generated with key
     """
-    return [["a"]]
+    grid = []
+    curRow = []
+    curIdx = 0
+    seenChars = set()
+    while curIdx < len(key):
+      if len(curRow) == 5:
+        grid.append(curRow)
+        curRow = []
+      curChar = key[curIdx]
+      if curChar in seenChars:
+        curIdx += 1
+        continue
+      seenChars.add(curChar)
+      curRow.append(curChar)
+      curIdx += 1
+    # Append the rest of the characters
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    curIdx = 0
+    while len(grid) != 5:
+      if len(curRow) == 5:
+        grid.append(curRow)
+        curRow = []
+      curChar = alphabet[curIdx]
+      if curChar in seenChars:
+        curIdx += 1
+        continue
+      seenChars.add(curChar)
+      curRow.append(curChar)
+      curIdx += 1
+    return grid
 
   def decode(self, message: str):
     """
@@ -23,6 +52,7 @@ class PlayFairCipherDecoder:
     returns:
       decoded_message (str): The decoded message
     """
+    print(self.pf_grid)
     return "init"
 
 message = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
