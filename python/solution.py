1a5f7a4d302688b62a73c28c4cedd4cece6a3c4f
Samyar Farjam <samyarfarjam@outlook.com>
2024-05-10 13:38:08 -0400

    Add bigram builder

--- python/solution.py
+++ python/solution.py
@@ -1,15 +1,15 @@
-
+from typing import Tuple, List
 class Cipher:
 
-    def __init__(self, key: str, cipher: str) -> None:
+    def __init__(self, key: str, cipherName: str) -> None:
         self.key = key
-        self.cipher = cipher
+        self.cipherName = cipherName
         self.gridSize = 5
         self.cipherGrid = None
         self.__buildPlayfairGrid()
 
     def __buildPlayfairGrid(self) -> None:
-        """ Build 5 x 5 Playfair grid using a key. """
+        """ Build 5 x 5 Play fair grid using the key. """
 
         # Remove duplicates from key
         seenCharacters = set()
@@ -29,11 +29,57 @@ class Cipher:
         self.cipherGrid = [list(flattenedGrid[i: i + self.gridSize]) for i in range(0, self.gridSize * self.gridSize, self.gridSize)]
 
 
+    def decrypt(self, encryptedMessage: str) -> None:
+        """
+        Decrypt an encrypted message given a key.
+        Args:
+            encryptedMessage: encrypted message to be decrypted.
+
+        Returns:
+            decryptedMessage: decrypted message.
+        """
+
+        decryptedMessage = ''
+        
+        bigrams = self.__buildBigrams(encryptedMessage)
+
+        
+
+
+
+    def __buildBigrams(self, text: str) -> List[Tuple[str, str]]:
+        """
+        Creates bigrams of characters from a text.
+        Args:
+            text: string to be divided into bigrams of its characters.
+
+        Returns:
+            bigrams of text.
+        """
+
+        text = text.upper()
+        bigrams = []
+        i = 0
+        while i < len(text) - 1:
+            if text[i] == text[i + 1]:
+                bigrams.append((text[i], 'X'))
+                i += 1
+            else:
+                bigrams.append((text[i], text[i + 1]))
+                i += 2
+        # Check if last character was not considered
+        if i == len(text) - 1:
+            bigrams.append((text[i], 'X'))
+        
+        return bigrams
+
+
 
 if __name__ == '__main__':
     encryptedMessage = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
     key = "SUPERSPY"
-    cipher = "Playfair"
+    cipherName = "Playfair"
 
-    cipher = Cipher(key=key, cipher=cipher)
-    print(cipher.cipherGrid)
+    cipher = Cipher(key=key, cipherName=cipherName)
+
+    message = cipher.decrypt(encryptedMessage)
