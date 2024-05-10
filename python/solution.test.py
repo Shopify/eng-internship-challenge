a3d542e1fc0f33dd342a17c75272d06335613643
boatnoah <noahskim7@gmail.com>
2024-05-09 23:34:27 -0700

    completed cipher

--- python/solution.test.py
+++ python/solution.test.py
@@ -1,15 +1,18 @@
-import unittest
-import subprocess
 import os
+import subprocess
+import unittest
+
 
 class TestSolution(unittest.TestCase):
     def test_decryption_output(self):
         # Run the solution.py script
-        result = subprocess.run(['python3', 'solution.py'], text=True, capture_output=True)
-        
+        result = subprocess.run(
+            ["python3", "solution.py"], text=True, capture_output=True
+        )
         # Check the output
-        expected_output = 'HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA'
+        expected_output = "HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
         self.assertEqual(result.stdout.strip(), expected_output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
