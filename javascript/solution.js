40fe1800e3b5fc72d3b74e91d9fefba10c28798f
Andrew Fenton <andrewfenton898@gmail.com>
2024-05-10 00:15:48 -0400

    working solution

--- javascript/solution.js
+++ javascript/solution.js
@@ -1,18 +1,31 @@
 const encryptedText = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV";
-const splitEncText = "IK EW EN EN XL NQ LP ZS LE RU MR HE ER YB OF NE IN CH CV";
 const answerText = "HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA";
 const cipherKey = "SUPERSPY";
 
+// Original keyTable Matrix
+// const keyTable = [
+//     ["S", "U", "P", "E", "R"],
+//     ["Y", "A", "B", "C", "D"],
+//     ["F", "G", "H", "I", "K"],
+//     ["L", "M", "N", "O", "Q"],
+//     ["T", "V", "W", "X", "Z"]
+// ];
+
+/**
+ * Transposed keyTable for coordinate system to be in format: [x][y].
+ * Otherwise, coordinates are flipped.
+ */
 const keyTable = [
-    ["S", "U", "P", "E", "R"],
-    ["Y", "A", "B", "C", "D"],
-    ["F", "G", "H", "I", "K"],
-    ["L", "M", "N", "O", "Q"],
-    ["T", "V", "W", "X", "Z"]
+    ["S", "Y", "F", "L", "T"],
+    ["U", "A", "G", "M", "V"],
+    ["P", "B", "H", "N", "W"],
+    ["E", "C", "I", "O", "X"],
+    ["R", "D", "K", "Q", "Z"]
 ];
 
 /**
- * Represents the key table:
+ * Represents the key table where letters are mapped to
+ * coordinates on 5x5 grid:
  *      S U P E R
  *      Y A B C D
  *      F G H I K
@@ -40,20 +53,23 @@ const keyTableMap = {
     "N": [2, 3],
     "O": [3, 3],
     "Q": [4, 3],
-    "T": [0, 3],
-    "V": [1, 3],
-    "W": [2, 3],
-    "X": [3, 3],
-    "Z": [4, 3]
+    "T": [0, 4],
+    "V": [1, 4],
+    "W": [2, 4],
+    "X": [3, 4],
+    "Z": [4, 4]
 };
 
 /**
  * Decrypts playfair cipher encrypted text.
- * @param {*} encryptedText 
+ * @param {String} encryptedText 
  * @returns String - decrypted text
  */
 function playfairDecryptor(encryptedText) {
-    return "";
+    const splitTextArr = splitEncText(encryptedText);
+    const decryptedText = decrypt(splitTextArr).toUpperCase();
+    const decryptedTextNoX = decryptedText.replace(/X/g, "");
+    console.log(decryptedTextNoX);
 }
 
 /**
@@ -62,7 +78,14 @@ function playfairDecryptor(encryptedText) {
  * @returns String[]
  */
 function splitEncText(text) {
-    return [];
+    const pairs = [];
+
+    // Encrypted text should always have even length.
+    for (let i = 0; i < text.length; i += 2) {
+        pairs.push(text.substring(i, i + 2));
+    }
+
+    return pairs;
 }
 
 
@@ -72,13 +95,30 @@ function splitEncText(text) {
  * @returns String - decrypted string
  */
 function decrypt(encTextArr) {
-    /**
-     * 1. If same row, get the letters (x_1 - 1, y_1), (x_2 - 1, y_2), wrapping around
-     * 2. If same column, get the letters (x_1, y_1 - 1), (x_2, y_2 - 1), wrapping around
-     * 3. If same column, get the letters (x_1, y_1), (x_2, y_2) --> (x_2, y_1), (x_1, y_2)
-     */
+    let decryptedText = "";
 
-    return "";
+    for (const pair of encTextArr) {
+        let decryptedPair = "";
+
+        const letterPair = pair.split("");
+        const firstCoords = keyTableMap[letterPair[0]];
+        const secondCoords = keyTableMap[letterPair[1]];
+
+        if (firstCoords[0] === secondCoords[0]) {
+            // handle column case
+            decryptedPair = decryptColumnRule(letterPair[0], letterPair[1]);
+        } else if (firstCoords[1] === secondCoords[1]) {
+            // handle row case
+            decryptedPair = decryptRowRule(letterPair[0], letterPair[1]);
+        } else {
+            // handle rectangle case
+            decryptedPair = decryptRectRule(letterPair[0], letterPair[1]);
+        }
+
+        decryptedText += decryptedPair;
+    }
+
+    return decryptedText;
 }
 
 /**
@@ -87,7 +127,21 @@ function decrypt(encTextArr) {
  * @returns String - decoded letter pair
  */
 function decryptColumnRule(l1, l2) {
-    return "";
+    const firstCoords = keyTableMap[l1];
+    const secondCoords = keyTableMap[l2];
+    let newFirstCoords = [firstCoords[0], firstCoords[1] - 1];
+    let newSecondCoords = [secondCoords[0], secondCoords[1] - 1];
+
+    // Wrap around
+    if (newFirstCoords[1] < 0) {
+        newFirstCoords[1] = 4;
+        newSecondCoords[1] = 4;
+    }
+
+    const decFirstLetter = keyTable[newFirstCoords[0]][newFirstCoords[1]];
+    const decSecondLetter = keyTable[newSecondCoords[0]][newSecondCoords[1]];
+    
+    return decFirstLetter + decSecondLetter;
 }
 
 /**
@@ -96,7 +150,21 @@ function decryptColumnRule(l1, l2) {
  * @returns String - decoded letter pair
  */
 function decryptRowRule(l1, l2) {
-    return "";
+    const firstCoords = keyTableMap[l1];
+    const secondCoords = keyTableMap[l2];
+    let newFirstCoords = [firstCoords[0] - 1, firstCoords[1]];
+    let newSecondCoords = [secondCoords[0] - 1, secondCoords[1]];
+
+    // Wrap around
+    if (newFirstCoords[0] < 0) {
+        newFirstCoords[0] = 4;
+        newSecondCoords[0] = 4;
+    }
+
+    const decFirstLetter = keyTable[newFirstCoords[0]][newFirstCoords[1]];
+    const decSecondLetter = keyTable[newSecondCoords[0]][newSecondCoords[1]];
+    
+    return decFirstLetter + decSecondLetter;
 }
 
 /**
@@ -104,5 +172,17 @@ function decryptRowRule(l1, l2) {
  * @returns String - decoded letter pair
  */
 function decryptRectRule(l1, l2) {
-    return "";
+    const firstCoords = keyTableMap[l1];
+    const secondCoords = keyTableMap[l2];
+
+    // Flip x-coordinates
+    let newFirstCoords = [secondCoords[0], firstCoords[1]];
+    let newSecondCoords = [firstCoords[0], secondCoords[1]];
+
+    const decFirstLetter = keyTable[newFirstCoords[0]][newFirstCoords[1]];
+    const decSecondLetter = keyTable[newSecondCoords[0]][newSecondCoords[1]];
+    
+    return decFirstLetter + decSecondLetter;
 }
+
+playfairDecryptor(encryptedText);
