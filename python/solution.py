3c5d54ea026977ce45f4c235178dfe0888a7075c
stbaraza <stbaraza@uwaterloo.ca>
2024-05-11 10:20:53 -0400

    Get filtered cipherkey

--- python/solution.py
+++ python/solution.py
@@ -1,3 +1,15 @@
+def decryptMsg(encryptedMsg, cipherKey):
+    # Construct 5x5 matrix and ensure removal of duplicate letters/non alphabetical letters
+    charSet = set()
+    filteredKey = []
+    for char in cipherKey:
+        if char not in charSet:
+            charSet.add(char)
+            filteredKey.append(char)
+    filteredKey = ''.join(filteredKey)
+    print(filteredKey)
+
+
 def main():
     # Variables
     encryptedMsg = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
@@ -13,10 +25,11 @@ def main():
     # # # # # # 
     # S U P E R                 # IK EW EN EN XL NQ LP ZS LE RU MR HE ER YB OF NE IN CH CV
     # Y A B C D                 # HI PX PO PO TO MO NS TR OS ES QU IP PE DA LI OP HO BI AX
-    # F G H I K                 # HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA
+    # F G H I K                 # HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA - decrypted message
     # L M N O Q
     # T V W X Z
 
+    decryptMsg(encryptedMsg, cipherKey)
 
 
 if __name__ == "__main__":
