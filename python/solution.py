04f4f3e31bf2b8c34c1117c3eabc81828bf85610
stbaraza <stbaraza@uwaterloo.ca>
2024-05-11 10:04:45 -0400

    Manual solution in comments, next step to code decryption function

--- python/solution.py
+++ python/solution.py
@@ -1,6 +1,23 @@
 def main():
+    # Variables
     encryptedMsg = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
     cipherKey = "SUPERSPY"
+
+    # Plan: The playfair cipher has an encryption and decryption process, from understanding the 
+    # encryption process we can proceed to decrypting it.
+
+    # The decryption process begins by grouping up the encrypted message into pairs of letters, then reversing encryption. 
+    # We also need to then place the encrypted message into a 5*5 matrix, and utilize the 3 encryption rules to undo the process
+
+    # First manually decrypt the message, then code it. Drop the J and make the matrix with superspy
+    # # # # # # 
+    # S U P E R                 # IK EW EN EN XL NQ LP ZS LE RU MR HE ER YB OF NE IN CH CV
+    # Y A B C D                 # HI PX PO PO TO MO NS TR OS ES QU IP PE DA LI OP HO BI AX
+    # F G H I K                 # HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA
+    # L M N O Q
+    # T V W X Z
+
+
 
 if __name__ == "__main__":
     main()
