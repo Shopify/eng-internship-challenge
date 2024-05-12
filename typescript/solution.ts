da862214e0b06597b9e3d26daf9f07cb777483fe
rohullahN <41601768+rohullahN@users.noreply.github.com>
2024-05-11 20:34:26 -0400

    implement lookup table for letters

--- typescript/solution.ts
+++ typescript/solution.ts
@@ -13,37 +13,27 @@ interface LetterIndex {
 }
 
 function generateMatrix(): string[][] {
-  const matrix = Array(5)
-    .fill(null)
-    .map(() => Array(5).fill(null));
-  const noDuplicates = Array.from(new Set(cipherKey + alphabetsWithoutJ));
-  let k = 0;
+  const matrix: string[][] = [];
+  const lettersWithoutDuplicates = Array.from(
+    new Set(cipherKey + alphabetsWithoutJ)
+  );
+  let index = 0;
 
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      matrix[i][j] = noDuplicates[k];
-      k++;
+  for (let char of lettersWithoutDuplicates) {
+    let rowIndex = Math.floor(index / 5);
+    let colIndex = index % 5;
+    if (matrix.length <= rowIndex) {
+      matrix.push([]);
     }
+    matrix[rowIndex][colIndex] = char;
+    lookup[char] = { row: rowIndex, column: colIndex };
+    index++;
   }
 
   return matrix;
 }
 
 const matrix = generateMatrix();
-
-function findIndex(letter: string) {
-  let index = { row: 0, column: 0 };
-
-  matrix.forEach((row) => {
-    row.forEach((col) => {
-      if (col === letter) {
-        index.row = matrix.indexOf(row);
-        index.column = row.indexOf(col);
-      }
-    });
-  });
-  return index;
-}
 
 function handleSameRow(index1: LetterIndex, index2: LetterIndex) {
   decryptedMessage +=
@@ -62,8 +52,8 @@ function handleSamecolumn(index1: LetterIndex, index2: LetterIndex) {
 }
 
 pairs.forEach((pair) => {
-  const indexOfL1 = findIndex(pair[0]);
-  const indexOfL2 = findIndex(pair[1]);
+  const indexOfL1 = lookup[pair[0]];
+  const indexOfL2 = lookup[pair[1]];
 
   if (indexOfL1.row === indexOfL2.row) {
     handleSameRow(indexOfL1, indexOfL2);
