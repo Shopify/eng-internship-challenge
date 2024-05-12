691de21b29986b6c98066a8f5bb09d9d851278a6
Sumit Mansukhlal Savaliya <savaliyasumit717@gmail.com>
2024-05-12 15:08:46 -0300

    feat: implemented function to create pairs

--- javascript/solution.js
+++ javascript/solution.js
@@ -15,3 +15,31 @@ function generateCipherMatrix(key) {
 
     return matrix;
 }
+
+/**
+ * Splits the encrypted text into pairs of characters.
+ * If the number of characters is odd, adds 'X' to make pairs.
+ * 
+ * @param {string} encryptedText - The text to be split into pairs.
+ * @returns {Array<Array<string>>} The array of character pairs.
+ */
+function createPairs(encryptedText) {
+    const pairs = [];
+
+    for (let i = 0; i < encryptedText.length; i += 2) {
+        // Get the first character of the pair
+        let firstChar = encryptedText[i];
+        // Determine the second character of the pair
+        let secondChar = (i + 1 < encryptedText.length && encryptedText[i] === encryptedText[i + 1]) ? 'X' : encryptedText[i + 1];
+        // Add the pair to the pairs array
+        pairs.push([firstChar, secondChar]);
+    }
+
+    // If the length of the encrypted text is odd, add the last character with 'X'
+    if (encryptedText.length % 2 !== 0) {
+        pairs.push([encryptedText[encryptedText.length - 1], 'X']);
+    }
+
+    return pairs;
+}
+
