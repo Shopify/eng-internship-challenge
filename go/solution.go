4a96d11a6b7fc9c2fc6a6c3681c10bd69491088d
gastraderr <gaslimits@gmail.com>
2024-05-13 18:23:14 -0400

    add modularity + readability

--- go/solution.go
+++ go/solution.go
@@ -6,49 +6,103 @@ import (
 )
 
 const (
-	Size = 5
+	SIZE = 5
 )
 
-func createKeySquare(key string) [Size][Size]rune {
-	var square [Size][Size]rune
+type PlayfairCipher struct {
+	Message         string
+	Keyword         string
+	LettersLocation map[rune]*LetterLocation
+	Matrix          PlayfairMatrix
+}
+
+type PlayfairMatrix [][]rune
+
+type LetterLocation struct {
+	X, Y int
+}
+
+func NewLetterLocation(x, y int) *LetterLocation {
+	return &LetterLocation{
+		X: x,
+		Y: y,
+	}
+}
+
+// creates a new PlayfairCipher instance and populates the matrix and letter locations
+func NewPlayfairCipher(msg, kw string) (*PlayfairCipher, error) {
+	msg = strings.ToUpper(msg)
+	kw = strings.ToUpper(kw)
+	cipher := &PlayfairCipher{
+		Message:         msg,
+		Keyword:         kw,
+		LettersLocation: make(map[rune]*LetterLocation),
+		Matrix:          createEmptyMatrix(),
+	}
+	if err := cipher.validateInput(); err != nil {
+		return nil, err
+	}
+	cipher.populateMatrix()
+	cipher.populateLetterLocations()
+	return cipher, nil
+}
+
+// initializes an empty Playfair matrix
+func createEmptyMatrix() PlayfairMatrix {
+	matrix := make(PlayfairMatrix, SIZE)
+	for i := range matrix {
+		matrix[i] = make([]rune, SIZE)
+	}
+	return matrix
+}
+
+// fills the Playfair matrix with the keyword and remaining alphabet letters
+func (p *PlayfairCipher) populateMatrix() {
 	used := make(map[rune]bool)
 	row, col := 0, 0
 
-	// add the key to the square
-	for _, char := range strings.ToUpper(key) {
-		if char == 'J' { //'J' is merged with 'I'
+	for _, char := range p.Keyword {
+		if char == 'J' {
 			char = 'I'
 		}
 		if !used[char] && char >= 'A' && char <= 'Z' {
 			used[char] = true
-			square[row][col] = char
+			p.Matrix[row][col] = char
 			col++
-			if col == Size {
+			if col == SIZE {
 				row++
 				col = 0
 			}
 		}
 	}
 
-	// fill remaining spaces in the square
 	for char := 'A'; char <= 'Z'; char++ {
-		if char == 'J' { // Skip 'J'
+		if char == 'J' {
 			continue
 		}
 		if !used[char] {
-			square[row][col] = char
+			p.Matrix[row][col] = char
 			col++
-			if col == Size {
+			if col == SIZE {
 				row++
 				col = 0
 			}
 		}
 	}
-
-	return square
 }
 
-func cleanText(input string) string {
+// creates a mapping of each letter to its location in the matrix
+func (p *PlayfairCipher) populateLetterLocations() {
+	for r := 0; r < SIZE; r++ {
+		for c := 0; c < SIZE; c++ {
+			char := p.Matrix[r][c]
+			p.LettersLocation[char] = NewLetterLocation(r, c)
+		}
+	}
+}
+
+// removes 'X', spaces and special characters from the input text
+func (p *PlayfairCipher) cleanText(input string) string {
 	input = strings.ReplaceAll(input, "X", "")
 	input = strings.ReplaceAll(input, " ", "")
 	return strings.Map(func(r rune) rune {
@@ -59,46 +113,56 @@ func cleanText(input string) string {
 	}, input)
 }
 
-func decryptPlayfair(cipherText, key string) string {
-	square := createKeySquare(key)
-	mapping := make(map[rune][2]int)
-	// precompute positions in map
-	for r := 0; r < Size; r++ {
-		for c := 0; c < Size; c++ {
-			mapping[square[r][c]] = [2]int{r, c}
-		}
-	}
-
+// decrypts the ciphertext using the Playfair cipher
+func (p *PlayfairCipher) decrypt() string {
 	var plainText string
-	cipherText = strings.ToUpper(cipherText)
-	// decrypt digraphs
+	cipherText := p.Message
+
 	for i := 0; i < len(cipherText); i += 2 {
 		char1 := rune(cipherText[i])
 		char2 := rune(cipherText[i+1])
-		pos1 := mapping[char1]
-		pos2 := mapping[char2]
+		pos1 := p.LettersLocation[char1]
+		pos2 := p.LettersLocation[char2]
 
-		// check if same row
-		if pos1[0] == pos2[0] {
-			char1 = square[pos1[0]][(pos1[1]-1+Size)%Size]
-			char2 = square[pos2[0]][(pos2[1]-1+Size)%Size]
-		} else if pos1[1] == pos2[1] { // check if same column
-			char1 = square[(pos1[0]-1+Size)%Size][pos1[1]]
-			char2 = square[(pos2[0]-1+Size)%Size][pos2[1]]
+		// check for same row
+		if pos1.X == pos2.X {
+			char1 = p.Matrix[pos1.X][(pos1.Y-1+SIZE)%SIZE]
+			char2 = p.Matrix[pos2.X][(pos2.Y-1+SIZE)%SIZE]
+		} else if pos1.Y == pos2.Y { // check for same column
+			char1 = p.Matrix[(pos1.X-1+SIZE)%SIZE][pos1.Y]
+			char2 = p.Matrix[(pos2.X-1+SIZE)%SIZE][pos2.Y]
 		} else { // rectangle
-			char1 = square[pos1[0]][pos2[1]]
-			char2 = square[pos2[0]][pos1[1]]
+			char1 = p.Matrix[pos1.X][pos2.Y]
+			char2 = p.Matrix[pos2.X][pos1.Y]
 		}
 
 		plainText += string(char1) + string(char2)
 	}
-	plainText = cleanText(plainText)
+	plainText = p.cleanText(plainText)
 	return plainText
+}
+
+func (p *PlayfairCipher) validateInput() error {
+	if len(p.Message) == 0 || len(p.Message)%2 != 0 {
+		return fmt.Errorf("cipher text length must be even and not zero")
+	}
+	if len(p.Keyword) == 0 || len(p.Keyword) > 25 {
+		return fmt.Errorf("keyword length must be between 1 and 25 characters")
+	}
+	return nil
 }
 
 func main() {
 	cipherText := "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
 	key := "SUPERSPY"
-	plainText := decryptPlayfair(cipherText, key)
+
+	pfCipher, err := NewPlayfairCipher(cipherText, key)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	plainText := pfCipher.decrypt()
+
 	fmt.Println(plainText)
 }
