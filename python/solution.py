25a31d9dbe98843682d76db9b6e004a64d5b495e
hamzaiqbal10 <20hi4@queensu.ca>
2024-05-13 19:42:53 -0400

    excluding letter J

--- python/solution.py
+++ python/solution.py
@@ -1,15 +1,15 @@
 def create_matrix(key):
-    #remove duplicate letters and convert to uppercase
-    filtered_key = ''.join(sorted(set(key.upper()), key = key.index))
-    #add the remianing letters of the alphabet
+    # Remove duplicate letters, convert to uppercase
+    filtered_key = ''.join(sorted(set(key.upper()), key=key.index))
+    # Add the remaining letters of the alphabet, excluding 'J'
     alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
-    for char in alphabet:
-        if char not in filtered_key:
-            filtered_key += char
-    # create the 5x5 matrix
-    matrix = [filtered_key[i:i+5] for i in range(0,25,5)]
+    filtered_key += ''.join([char for char in alphabet if char not in filtered_key and char != 'J'])
+    # Create the 5x5 matrix
+    matrix = [filtered_key[i:i+5] for i in range(0, 25, 5)]
     return matrix
 
 key = "SUPERSPY"
-matrix = create_index_matrix(key)
+matrix = create_matrix(key)  # Corrected function call
 print("Matrix:\n", "\n".join([" ".join(row) for row in matrix]))
+
+
