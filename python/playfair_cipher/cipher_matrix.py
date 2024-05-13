0b2f69f88875a832f3ef39b906d63965df84b5a5
Rashmi Kumari <rashmi25346@gmail.com>
2024-05-13 13:06:07 -0400

    Adding comments

--- python/playfair_cipher/cipher_matrix.py
+++ python/playfair_cipher/cipher_matrix.py
@@ -1,18 +1,22 @@
 import string
 
+"""The Cipher Matrix includes a 2D list with the Cipher Key's letters and non-repeating alphabet letters."""
 class CipherMatrix:
   def __init__(self,cipherkey):
     self.matrix = []
     self.cipher_key = cipherkey
     self.__create()
 
-  def __create(self):
-    cipher_chars = self.__get_all_cipher_chars()
-    self.__populate_matrix(cipher_chars)
-
+  """Gets the matrix"""
   def get(self):
     return self.matrix
   
+  """Gets cipher chars and creates a 2D 5x5 matrix"""
+  def __create(self):
+    cipher_chars = self.__get_all_cipher_chars()
+    self.__populate_matrix(cipher_chars)
+  
+  """Populates 2D cipher matrix with cipher chars"""
   def __populate_matrix(self, cipher_chars):
     letter_count = 0
     for _ in range(5):
@@ -22,16 +26,19 @@ class CipherMatrix:
         letter_count += 1
       self.matrix.append(matrix_row)
 
+  """Gets all characters for cipher matrix."""  
   def __get_all_cipher_chars(self):
     cipher_chars = []
     for each_char in self.cipher_key:
       if each_char not in cipher_chars:
         cipher_chars.append(each_char.upper())
-    
+
     for each_letter in string.ascii_uppercase:
       if each_letter not in cipher_chars and each_letter not in ('I', 'J'):
         cipher_chars.append(each_letter)
+      # For 'I' and 'J', 'I/J' is stored in the Cipher Matrix to accommodate its 25-character limit.
       elif 'I/J' not in cipher_chars and each_letter  in ('I', 'J'):
         cipher_chars.append('I/J')
+
     return cipher_chars
   
--- python/playfair_cipher/decryption_service.py
+++ python/playfair_cipher/decryption_service.py
@@ -1,15 +1,24 @@
 from playfair_cipher.cipher_matrix import CipherMatrix
 
+"""Stores position of a character in Cipher Matrix"""
 class CharacterPosition:
   def __init__(self, row: int, column: int):
     self.row = row
     self.column = column
 
+"""This class comprises functions designed for decryption of an encrypted message."""
 class DecryptionService:
   def __init__(self, cipher_key: str) -> None:
     self.cipher_key = cipher_key
     self.cipher_matrix = CipherMatrix(cipher_key).get()
 
+  """This function will  :
+
+  1. Generates character pairs from the encrypted message.
+  2. Finds the indices of the decrypted message in the cipher matrix.
+  3. Retrieves the decrypted message from the character positions.
+  4. Reveals the final decrypted message.
+  """
   def decrypt(self, message: str) -> str:
     character_pairs = self.__generate_character_pairs(message)
     decrypted_message_char_positions = self.__find_decrypted_message_char_positions(character_pairs)
@@ -18,6 +27,8 @@ class DecryptionService:
 
     return revealed_decrypted_message
 
+  """This function generates pairs for decrypting the provided message. 
+    It adds 'X' if the pair consists of the same character and returns a list of pairs.""" 
   def __generate_character_pairs(self, message: str) -> list[str]:
     character_pairs = []
     current_pair = ""
@@ -36,32 +47,42 @@ class DecryptionService:
 
     return character_pairs
   
+  """This function returns the indices of a character from the Cipher Matrix."""
   def __find_position_of_character(self, character: str) -> CharacterPosition:
     for row in range(0,5):
       for column in range(0,5):
-        if self.cipher_matrix[row][column] == character:
-          return CharacterPosition(row, column)
-        elif character in ('I', 'J') and self.cipher_matrix[row][column] == 'I/J':
+        char_at_current_position = self.cipher_matrix[row][column]
+
+        if char_at_current_position == character or (
+          character in ('I', 'J') and char_at_current_position == 'I/J'
+        ):
           return CharacterPosition(row, column)
         
+  """Returns character at a given position."""
   def __find_character_at_postition(self, character_position: CharacterPosition):
     cipher_char = self.cipher_matrix[character_position.row][character_position.column]
+    
+    # This will returns 'I' for both 'I' and 'J', treating them as equivalent.
     if cipher_char == 'I/J':
       cipher_char = 'I'
     return cipher_char
   
+  """Gets position of the character to swap if both characters in the pair are in same column."""
   def __get_same_column_decrypted_position(self, message_char: CharacterPosition):
     return CharacterPosition((message_char.row - 1) % 5 , message_char.column)
-
+  
+  """Gets position of the character to swap if both characters in the pair are in same rows."""
   def __get_same_row_decrypted_position(self, message_char: CharacterPosition):
     return CharacterPosition(message_char.row, (message_char.column - 1) % 5)
   
+  """Gets position of the character to swap if both characters in the pair forms a rectangle."""
   def __get_rectangle_decrypted_position(self, first_message_char: CharacterPosition, second_message_char: CharacterPosition):
     return [
       CharacterPosition(first_message_char.row, second_message_char.column),
       CharacterPosition(second_message_char.row, first_message_char.column)
     ]
 
+  """Gets position of the character to swap for decryption for all message pairs."""
   def __find_decrypted_message_char_positions(self, character_pairs: list[str]) -> list[CharacterPosition]:
     decrypted_message_char_positions=[]
 
@@ -84,6 +105,7 @@ class DecryptionService:
 
     return decrypted_message_char_positions
   
+  """This will return the decrypted message, including any 'X' characters added during pair generation."""
   def __get_message_from_char_positions(self, decrypted_message_char_positions: list[CharacterPosition]) -> str:
     decrypted_message = ""
 
@@ -96,13 +118,15 @@ class DecryptionService:
 
     return decrypted_message
   
+  """This function removes any 'X' characters added during pair generation to obtain the original decrypted message."""
   def __remove_fillers_from_decrypted_message(self, decrypted_message: str) -> str:
     index = 1
     while index < len(decrypted_message) - 1:
       if decrypted_message[index] == "X" and decrypted_message[index-1] == decrypted_message[index+1]:
         decrypted_message = decrypted_message.replace(decrypted_message[index], "")
       index += 1
-   
+
+    # check for  extra 'X' char at the end of decrypted message
     if decrypted_message[-1] == "X" and len(decrypted_message) % 2 == 0:
       decrypted_message = decrypted_message[:-1]
 
