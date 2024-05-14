e9b8935b03d1794be57206e816f121d40edcd770
yili288 <ngyili28@hotmail.com>
2024-05-14 01:38:14 -0400

    types, docstrings, generate key table func and code clean up

--- python/solution.py
+++ python/solution.py
@@ -1,27 +1,35 @@
+import string 
+
 TABLE_SIZE = 5 
-encrpted_msg = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
-key = "SUPERSPY"
 
-key_table = []
+def generate_key_table(key: str) -> list[list[str]]:
+    """Generate a key table based on the given key"""
+    key = key + string.ascii_uppercase
+    key = key.replace("J", "") # remove J to fit the alphabet
+    key = "".join(dict.fromkeys(key)) # remove duplicates
 
-# omit "J" or "Q" to reduce the alphabet to fit
-key_table = [['S','U','P','E','R'], 
-             ['Y','A','B','C','D'], 
-             ['F','G','H','I','K'], 
-             ['L','M','N','O','Q'], 
-             ['T','V','W','X','Z']]
+    if len(key) == TABLE_SIZE * TABLE_SIZE:
+      # Create empty 5 x 5 table
+      key_table = [['' for x in range(TABLE_SIZE)] for y in range(TABLE_SIZE)]
+      k = 0
+      for i in range(TABLE_SIZE):
+          for j in range(TABLE_SIZE):
+              key_table[i][j] = key[k]
+              k += 1
+      return key_table
+    else:
+        # Key is not valid
+        return None
 
-# param: encrypted message
-# return: ['AB','CD']
-def split_into_pairs(encrpted_msg):
+def split_into_pairs(encrpted_msg: str) -> list[str]:
+    """Split the encrypted message into pairs of two"""
     pairs = []
     for i in range(0, len(encrpted_msg), 2):
         pairs.append(encrpted_msg[i:i+2])
     return pairs
 
-# param: 'AB'
-# return: rowA colA rowB colB [1,2,3,4]
-def find_row_col(pair):
+def find_row_col(pair: str) -> list[int]:
+    """Find the row and column of the pair in the key table"""
     row_col = []
     for letter in pair:
         for i in range(TABLE_SIZE):
@@ -31,23 +39,21 @@ def find_row_col(pair):
                     row_col.append(j)
     return row_col
 
-# param: rowA colA rowB colB [1,2,3,4]
-# return: boolean
-def same_row(row_col):
+def same_row(row_col: list[int]) -> bool:
+    "Check if the pair is in the same row in the key table"
     if row_col[0] == row_col[2]:
         return True
     return False
 
-# param: rowA colA rowB colB [1,2,3,4]
-# return: boolean
-def same_column(row_col):
+def same_column(row_col : list[int]) -> bool:
+    "Check if the pair is in the same column in the key table"
     if row_col[1] == row_col[3]:
         return True
     return False
 
-# Playfair Cipher
-def decrypt(encrpted_msg, key):
-    pairs = split_into_pairs(encrpted_msg)
+def decrypt(encrypted_msg: str, key_table: list[list[str]]) -> str:
+    """Decrypt the encrypted message"""
+    pairs = split_into_pairs(encrypted_msg)
     
     decrypted_msg = ""
     for pair in pairs:
@@ -67,5 +73,11 @@ def decrypt(encrpted_msg, key):
     decrypted_msg = decrypted_msg.replace("X", "")
     return decrypted_msg
 
-# Answer
-print(decrypt(encrpted_msg, key))
+# Run the decryption
+if __name__ == "__main__":
+  encrypted_msg = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+  key = "SUPERSPY"
+
+  key_table = generate_key_table(key)
+  print(decrypt(encrypted_msg, key_table))
+  
