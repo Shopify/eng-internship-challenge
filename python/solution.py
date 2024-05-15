b04acf1e3ae818020bfd92ce6846762e2807bd02
justinjao <jao.c.justin@gmail.com>
2024-05-15 14:40:50 -0700

    Add dictionary of letter positions for faster position searching

--- python/solution.py
+++ python/solution.py
@@ -1,6 +1,7 @@
 cipher_text = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
 key = "SUPERSPY"
 # Construct the 5x5 grid
+letter_positions = {}
 
 
 def build_decryption_grid():
@@ -57,10 +58,43 @@ def build_decryption_grid():
 
                 if key_index < len(key):
                     decryption_grid[i][j] = key[key_index]
+                    letter_positions[key[key_index]] = [i, j]
                     remaining_letters.remove(key[key_index])
                     key_index += 1
                     continue
 
             # otherwise, add from remaining letters:
-            decryption_grid[i][j] = remaining_letters.pop(0)
+            next_letter = remaining_letters.pop(0)
+            decryption_grid[i][j] = next_letter
+            letter_positions[next_letter] = [i, j]
+
     print(decryption_grid)
+    print(letter_positions)
+
+
+build_decryption_grid()
+# def solve_message():
+#     index_1, index_2 = 0, 0
+#     while index_2 < len(cipher_text):
+#         # get new characters
+#         char_1 = cipher_text[index_1]
+#         char_2 = cipher_text[index_2]
+
+#         # determine which 3 cases
+
+#         # same row, get letters to the left
+#         if letter_positions[char_1][0] == letter_positions[char_2][0]:
+#             # determine if wrap-around needed
+#             asdf
+
+#         # same column, get letters above
+#         elif letter_positions[char_1][1] == letter_positions[char_2][1]:
+#             # determine if wrap-around needed
+#             asdf
+
+#         # diff row or column, need to get rectangle
+#         else:
+#             asdf
+
+#         index_1 += 1
+#         index_2 += 1
