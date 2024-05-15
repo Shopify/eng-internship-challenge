55686db8f2a76bd10e7df449ea4a80fb03bd4454
AMY <121129755+ameemee@users.noreply.github.com>
2024-05-14 23:28:34 -0400

    implemented logic

---
+++ javascript/solution.js
@@ -0,0 +1,131 @@
+/* 
+This solution will decrypt the cipherText into plainText in two parts:
+
+First, generating the 5*5 keytable.
+  - the table must not contain duplicate letters or non-letter characters
+  - all letters in the table must be uppercase
+
+Then, decrypt the cipherText using the keytable.
+The algorithm used is as follows:
+  For each digram, obtain the keyTable row and col of the 2 letters indiivdually.
+  - if (col1 === col2), replace each letter with the letter ABOVE it (wrapping index 0 --> 4)
+      - ie. letter1: keyTable[row1][(col1 + 4) % 5] & letter2: keyTable[row2][(col2 + 4) % 5]
+  - if (row1 === row2), replace each letter with the letter to its LEFT (wrapping index 0 --> 4)
+      - ie. letter1: keyTable[(row1 + 4) % 5][col1] & letter2: keyTable[(row2 + 4) % 5][col2]
+  - if neither, swap the columns of the 2 letters
+      - ie. letter1: keyTable[row1][col2] & letter2: keyTable[row2][col1]
+
+
+Rationale for representing the 5x5 keytable as a 2D vs. 1D array:
+  - 2D array is more intuitive to visualize and access
+  - Does not rely on math (ie. modulus) to denote element indices
+  - 2D array is more difficult to populate when generating keytable (looping vs. appending via .push())
+
+  However, 2D array was chosen since the keytable is generated only 
+  once yet indexed multiple times.
+*/
+
+// HELPER FUNCTIONS:
+function getLetterRowCol(letter, keyTable) {
+  for (row = 0; row < keyTable.length; row++) {
+    for (col = 0; col < keyTable[row].length; col++) {
+      if (keyTable[row][col] === letter) {
+        return [row, col];
+      }
+    }
+  }
+  return [null, null];
+}
+
+// PART 1: GENERATE THE 5*5 KEYTABLE
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function generateKeyTable(key) {
+  // ensure key is all uppercase and contains only unique letter characters
+  //  note: see Pull Request descrition for alternative ways to handle this: "alternate key generation methods"
+  //  note: an alternative way to uppercase all letters is to manipulate the ASCII
+  //  note: an alternative way to remove duplictes is to loop through and compare consecutive letters
+  //  note: an alternative way to regex-removing non-letter characters is to check the ASCII letter range
+  key = [...new Set(key.toUpperCase().replace(/[^A-Z]/g, ""))].join("");
+
+  // ensure the table is completed with remaining unique letters (not in the key)
+  let remainingUniqueLetters = alphabet;
+  for (let i = 0; i < key.length; i++) {
+    remainingUniqueLetters = remainingUniqueLetters.replace(key[i], "");
+  }
+
+  // remove one more letter (for a total of 25 letters)
+  const removableLetters = ["J", "Q", "I"];
+  for (let letter of removableLetters) {
+    if (remainingUniqueLetters.includes(letter)) {
+      remainingUniqueLetters = remainingUniqueLetters.replace(letter, "");
+      break;
+    }
+  }
+
+  // populate the 5x5 keytable (2D array) with key and remainingUniqueLetters
+  // [0, 1, 2, 3, 4, 5, ... , 23, 24] ==>
+  // [ [0, 1, 2, 3, 4],
+  //   [5, 6, 7, 8, 9],
+  //   [10, 11, 12, 13, 14],
+  //   [15, 16, 17, 18, 19],
+  //   [20, 21, 22, 23, 24] ];
+  let temporaryKeyTable = key
+    .split("")
+    .concat(remainingUniqueLetters.split(""));
+  let keyTable = [];
+  for (let i = 0; i < temporaryKeyTable.length; i += 5) {
+    keyTable.push(temporaryKeyTable.slice(i, i + 5));
+  }
+
+  return keyTable;
+}
+
+// PART 2: DECRYPT THE CIPHERTEXT USING THE KEYTABLE
+function decryptCipherText(cipherText, key) {
+  cipherText = cipherText.toUpperCase();
+  const keyTable = generateKeyTable(key);
+  let plainText = "";
+
+  // note: the cipherText will always have an even number of letters dy design
+  for (let digramIndex = 0; digramIndex < cipherText.length; digramIndex += 2) {
+    let letter1 = cipherText[digramIndex];
+    let letter2 = cipherText[digramIndex + 1];
+
+    let [row1, col1] = getLetterRowCol(letter1, keyTable);
+    let [row2, col2] = getLetterRowCol(letter2, keyTable);
+
+    // apply the decryption procedure as described in the PR description
+    if (col1 === col2) {
+      plainText += keyTable[(row1 + 4) % 5][col1];
+      plainText += keyTable[(row2 + 4) % 5][col2];
+    } else if (row1 === row2) {
+      plainText += keyTable[row1][(col1 + 4) % 5];
+      plainText += keyTable[row2][(col2 + 4) % 5];
+    } else {
+      plainText += keyTable[row1][col2];
+      plainText += keyTable[row2][col1];
+    }
+  }
+
+  // ensure decrypted string is: all uppercase & contains only letter characters & excludes 'X'
+  plainText = plainText
+    .toUpperCase()
+    .replace(/[^A-Z]/g, "")
+    .replace(/X/g, "");
+
+  return plainText;
+}
+
+// RUNNING THE ALGORITHM!
+const cipherText = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV";
+const key = "SUPERSPY";
+const plainText = decryptCipherText(cipherText, key);
+console.log(plainText);
+
+// ADDITIONAL SAMPLE TEST CASES:
+// console.log(generateKeyTable("monarchy"));
+// console.log(generateKeyTable("playfair example"));
+
+// console.log(decryptCipherText("gatlmzclrqtx", "SUPERSPY"));
+// console.log(decryptCipherText("gatlmzclrqtx", "monarchy"));
