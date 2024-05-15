d560b3ac981ce912e8aaa97838778905d3e8bf31
Cora <cora@dhcp-33-104.cu-wireless-faculty.carleton.ca>
2024-05-15 18:10:17 -0400

    add information

---
+++ python/solution.py
@@ -0,0 +1,16 @@
+# decrypted string must by entirely **UPPER CASE**, 
+# and not include `spaces`, the letter `"X"`, or `special characters`
+
+# encrypted message: IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV
+# key to the cipher: SUPERSPY
+
+'''
+    S U P E R 
+    Y A B C D
+    F G H I K   I=J
+    L M N O Q
+    T V W X Z
+'''
+
+# IK EW EN EN XL NQ LP ZS LE RU MR HE ER YB OF NE IN CH CV
+# HI P  PO
