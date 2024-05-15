b38af16561e4315fbb7255cb8928380288788631
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2024-05-15 17:48:50 -0600

    Add comments explaining each function

--- python/solution.py
+++ python/solution.py
@@ -3,6 +3,16 @@ FILLER_CHARACTER = 'X'
 ROWS = 5
 COLS = 5
 
+"""
+Creates a 5x5 key grid based on provided key and alphabet coordinates
+Grid is populated with alphabets from the key first, then with remaining alphabets (repeating occurrences not added)
+Args:
+    key: The symmetric cipher key string used for generating the key grrd
+Returns:
+    tuple:
+        key_grid: The generated 5x5 grid used for decryption
+        alphabet_coordinates: Dictionary that maps each alphabet to its (row,col) coordinates in the key grid
+"""
 def makeGrid(key: str):
     key_grid = [[0 for _ in range(ROWS)] for _ in range(COLS)]
     key_index = 0
@@ -30,6 +40,13 @@ def makeGrid(key: str):
 
     return key_grid, alphabet_coordinates
 
+"""
+Finds pairs("digrams") in an ecrypted message, adding each pair into the pairs array
+Args:
+    encrypted_message: Message encrypted with the PlayFair Cipher
+Returns:
+    pairs_array: A list of character pairs found in the encrypted message.
+"""
 def findPairs(encrypted_message: str):
     encrypted_message = encrypted_message.upper()
     pairs_array = []
@@ -39,6 +56,15 @@ def findPairs(encrypted_message: str):
         i += 2
     return pairs_array
 
+"""
+Decrypts a list of character pairs using a key grid and alphabet coordinates
+Args:
+    key_grid: A 2D list representing the key grid used for decryption
+    pairs_array: A list of alphabet pairs(tuples) to decrypt
+    alphabet_coordinates: A dictionary mapping each alphabet to its coordinates in the key grid
+Returns:
+    str: The decrypted plain text
+"""
 def decrypt(key_grid: list, pairs_array: list, alphabet_coordinates: dict):
     plain_text = []
     for pair in pairs_array:
@@ -58,7 +84,7 @@ def decrypt(key_grid: list, pairs_array: list, alphabet_coordinates: dict):
             second_alphabet_decrypted = key_grid[(second_alphabet_row_in_grid - 1) % 5][second_alphabet_col_in_grid]
             plain_text.append(first_alphabet_decrypted)
             plain_text.append(second_alphabet_decrypted)
-            
+
         else:
             first_alphabet_decrypted = key_grid[first_alphabet_row_in_grid][second_alphabet_col_in_grid]
             second_alphabet_decrypted = key_grid[second_alphabet_row_in_grid][first_alphabet_col_in_grid]
