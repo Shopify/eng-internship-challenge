78f630089e0ead09f01595cd600c78350ea6ad1b
baedirin <davisbr2@oregonstate.edu>
2024-05-15 07:10:39 -0700

    skeleton functions complete

--- python/solution.py
+++ python/solution.py
@@ -3,7 +3,39 @@
 # Shopify Internship Challenge
 
 # a function for creating the matrix
+def create_matrix(keyword):
+    """
+    A function which builds the
+    matrix by utilizing the given
+    keyword in a 5x5 fashion.
+    Returns the matrix as a list.
+    """
+
+    pass
+
 
 # a function for decryption
+def decrypt_message(message, matrix):
+    """
+    A function which decrypts
+    a given message, utilizing the
+    matrix position in pairs via indexing.
+    Returns the message as a string.
+    """
+
+    pass
+
 
 # a function for determining the position
+def matrix_position(matrix):
+    """
+    A function to determine
+    the position within the matrix,
+    utilizing a map of rows and columns.
+    Returns a dictionary of the map.
+    """
+
+    pass
+
+
+# test code
