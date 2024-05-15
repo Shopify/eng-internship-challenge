63e0f6d28f77716b01ef6dac3553f844c0e4268a
baedirin <davisbr2@oregonstate.edu>
2024-05-15 06:56:59 -0700

    beginning function structure

---
+++ python/solution.py
@@ -0,0 +1,9 @@
+# Brittaney Nicole Davis (Nico)
+# 5/14/2024
+# Shopify Internship Challenge
+
+# a function for creating the matrix
+
+# a function for decryption
+
+# a function for determining the position
