3fda360fdf5b6bf1cafa5669447a84cd17a57e15
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-05-16 10:15:49 -0400

    Bug fixes for playfair cipher

--- python/solution.py
+++ python/solution.py
@@ -12,15 +12,15 @@ def generate_grid(keyword):
 
 
 def decrypt_message(message, keyword):
-grid = generate_grid(keyword)
-message = message.upper().replace(" ", "")
-decrypted_message = ""
+    grid = generate_grid(keyword)
+    message = message.upper().replace(" ", "")
+    decrypted_message = ""
     i = 0
     while i < len(message) - 1:
         if message[i] == message[i + 1]:
             message = message[:i + 1] + 'X' + message[i + 1:]
         i += 2
-        if len(message) % 2 != 0:
+    if len(message) % 2 != 0:
         message += 'X'
     i = 0
     while i < len(message):
@@ -34,13 +34,10 @@ decrypted_message = ""
             if pair[1] in grid[row]:
                 pos2 = (row, grid[row].index(pair[1]))
 
-        
         if pos1[1] == pos2[1]:
             decrypted_pair = grid[(pos1[0] - 1) % 5][pos1[1]] + grid[(pos2[0] - 1) % 5][pos2[1]]
-        
         elif pos1[0] == pos2[0]:
             decrypted_pair = grid[pos1[0]][(pos1[1] - 1) % 5] + grid[pos2[0]][(pos2[1] - 1) % 5]
-       
         else:
             decrypted_pair = grid[pos1[0]][pos2[1]] + grid[pos2[0]][pos1[1]]
 
@@ -48,6 +45,7 @@ decrypted_message = ""
         i += 2
 
     return decrypted_message
+
 
 encrypted_message = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
 keyword = "SUPERSPY"
