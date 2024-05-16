f04b7807fc2cb9f8d480cc43d3e165fe48d8fff4
swhasans <syedwadoodhassan@gmail.com>
2024-05-15 23:36:35 -0400

    Added helper functions: toLowerCase, toUpperCase, removeSpaces

---
+++ javascript/solution.js
@@ -0,0 +1,11 @@
+function toLowerCase(msg) {
+    return msg.toLowerCase();
+}
+
+function toUpperCase(msg) {
+    return msg.toUpperCase();
+}
+
+function removeSpaces(msg) {
+    return msg.split(' ').join('');
+}
