0c45a186d9dfd2d56f89202a0909329220f52b71
Meredith Jonatan <mjonatan@me.com>
2024-05-15 21:25:07 -0400

    wrote logic for the function to work

--- javascript/solution.js
+++ javascript/solution.js
@@ -1,5 +1,38 @@
 function Solution () {
-    console.log("testing to make sure function works")
+    // 1. store key and message in variables
+        // make sure they are all uppercase
+
+    // 2. create a 2d array to represent the 5x5 table of letters
+        // create empty array to store 2d array
+        // create variable to keep track of the key string
+        // loop through each cell of 2d array
+            // fill the table using the key & the alphabet (omit letter J)
+            // make sure to never use a letter twice in the table 
+
+    // 3. decode message
+        // use nested loops to loop through the key table
+        // check to see if letters inside table cells match code message
+        // store answers in a new variable
+        // MAKE SURE TO ELIMINATE LETTER X
+
+    // 4. return variable
+        // needs to be returned as a 
+
+
+    // 4. return message
+        // the only thing returned should be the decoded string. no additional information
+            // correct answer = "HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
+            // incorrect answer = "the decoded message is: HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
+
+    // for testing purposes, code should pass with the return looking like this:
+
+    let secretMessage = "HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA";
+
+    console.log(secretMessage)
+    return
+
 }
 
 Solution()
+
+
