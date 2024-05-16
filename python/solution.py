3f077b94806857aa384123c15cf20e9076f1b0ef
pra-jna <prajna@umich.edu>
2024-05-15 23:01:52 -0400

    Decrypt message using key table

--- python/solution.py
+++ python/solution.py
@@ -2,7 +2,8 @@
 
 table = [['/' for _ in range(5)] for _ in range(5)]
 key = "SUPERSPY"
-encMessage = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+encMsg = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+
 def fillTable():
     # Fill table with key, omitting duplicates
     for i in range(5):
@@ -18,13 +19,65 @@ def fillTable():
             continue
         elif letter in key:
             continue
-        if curRow > 4:
-            print("ERROR")
-            break
         table[curRow][curCol] = letter
         curCol += 1
         if curCol > 4:
             curRow += 1
             curCol = 0
-fillTable()
-print(table)
+
+# Returns position of character in table
+def search(elem):
+    for i in range(5):
+        for j in range(5):
+            if (table[i][j] == elem):
+                return i,j
+
+def decrypt(msg):
+    fillTable()
+    # Split key into pairs
+    pairs = [msg[i:i + 2] for i in range(0, len(msg), 2)]
+
+    newPairs = []
+    for pair in pairs:
+        x1, y1 = search(pair[0])
+        x2, y2 = search(pair[1])
+
+        # 1) Both letters are in the same row of the key table
+        if x1 == x2:
+            if y1 - 1 < 0:
+                x1New, y1New = x1, 4
+            else:
+                x1New, y1New = x1, y1 - 1
+            if y2 - 1 < 0:
+                x2New, y2New = x2, 4
+            else:
+                x2New, y2New = x2, y2 - 1
+        
+        # 2) Both letters are in the same column of the key table
+        elif y1 == y2:
+            if x1 - 1 < 0:
+                x1New, y1New = 4, y1
+            else:
+                x1New, y1New = x1 - 1, y1
+            if x2 - 1 < 0:
+                x2New, y2New = 4, y2
+            else:
+                x2New, y2New = x2 - 1, y2
+        
+        # 3) In any other case, the letter is replaced by the letter in its own row and the column occupied by the other letter in its pair
+        else:
+            x1New, y1New = x1, y2
+            x2New, y2New = x2, y1
+
+        newPairs.append(table[x1New][y1New] + table[x2New][y2New])
+    msg = ''.join(newPairs)
+
+    # Remove additional Xs that are either in between a pair of letters or at the end of the message
+    for i in range(len(msg)-1):
+        if msg[i] == 'X' and i < len(msg) - 1 and msg[i-1] == msg[i+1]: 
+            msg = msg[:i] + msg[i+1:]
+    if msg[len(msg)-1] == 'X':
+        msg = msg[:len(msg)-1]
+    print(msg)
+
+decrypt(encMsg)
