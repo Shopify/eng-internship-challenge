aa4cd03e8215d02c1592de91b844c7ce03feebe1
Meredith Jonatan <mjonatan@me.com>
2024-05-15 22:48:01 -0400

    added logic to remove X from the final result, test is now passing

--- javascript/solution.js
+++ javascript/solution.js
@@ -1,15 +1,9 @@
 function Solution (playfairKey, playfairCodedMessage) {
-    // 1. store key and message in variables
-        // make sure they are all uppercase, no spaces
 
     let key = playfairKey.toUpperCase().split("").filter((letter, index, self) => self.indexOf(letter) === index).join("").split(" ").join("");
     let codedMessage = playfairCodedMessage.toUpperCase().split(" ").join("");
 
-    // 2. create a 2d array to represent the 5x5 table of letters
-
     let keyTable = generateKeyTable(key);
-
-    // 3. decode message
 
     let decodedMessage = "";
     for (let i = 0; i < codedMessage.length; i += 2) {
@@ -18,25 +12,14 @@ function Solution (playfairKey, playfairCodedMessage) {
         decodedMessage += decodedPair;
     }
 
+    decodedMessage = decodedMessage.replace(/X/g, '');
 
-        // use nested loops to loop through the key table
-        // check to see if letters inside table cells match code message
-        // store answers in a new variable
-        // MAKE SURE TO ELIMINATE LETTER X
-
-    // 4. return message
-        // the only thing returned should be the decoded string. no additional information
-            // correct answer = "HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
-            // incorrect answer = "the decoded message is: HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
-
-    // for testing purposes, code should pass with the return looking like this: console.log(decodedMessage)
-
-    console.log("key", key, "table:", keyTable, "🗝️: ", decodedMessage)
+    console.log(decodedMessage)
     return
 
 }
 
-// helper functions:
+// helper functions below:
 
 // this function generates the 5x5 key table
 function generateKeyTable(key) {
@@ -109,4 +92,5 @@ function decodePairs(pair1, pair2, keyTable) {
 
 Solution("SUPERSPY", "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV")
 
-
+"HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
+"HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA"
