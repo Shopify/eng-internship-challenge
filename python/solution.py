e61cc88f7ef4efde04c41ab5a99f99857bbb4e6e
Jian Li <jli4627@uwo.ca>
2024-05-15 21:07:32 -0400

    start and add header

---
+++ python/solution.py
@@ -0,0 +1,10 @@
+"""
+**********************************************
+Jian Li (j438li@uwaterloo.ca)
+Technical Challenges
+Python
+Decrypts messages encrypted using a Playfair cipher
+in order to access SpyCity.
+**********************************************
+"""
+
