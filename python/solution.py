d3f2ad48085862423f1d01d29b0ea0f27c1ae100
Jian Li <jli4627@uwo.ca>
2024-05-15 21:42:23 -0400

    Finish 2D matrix for decoding Playfair cipher

--- python/solution.py
+++ python/solution.py
@@ -8,3 +8,52 @@ in order to access SpyCity.
 **********************************************
 """
 
+
+
+def create_playfair_key_square(key):
+    """
+    create_playfair_key_square creates a 5x5 Playfair cipher key square based on the key.
+    Requires: key (str): Must be a sequence of alphabet characters.
+    
+    Returns:
+    list: A list of lists representing the 5x5 2D matrix.
+ 
+    Example:
+    >>> create_playfair_key_square("KEYWORD")
+    [['K', 'E', 'Y', 'W', 'O'], ['R', 'D', 'A', 'B', 'C'], ...]
+    """
+    alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+
+   #Remove duplicates
+    unique_characters = []
+    seen = set()
+    for char in key:
+        if char not in seen:
+            seen.add(char)
+            unique_characters.append(char)
+    
+    #Convert to uppercase
+    unique_characters = [char.upper() for char in unique_characters]
+    
+    #Replace J with I
+    unique_characters = [char.replace('J', 'I') for char in unique_characters]
+    
+    #Join characters into a single string
+    key = ''.join(unique_characters)
+    
+    #Combine key with alphabet
+    used = set(key)
+    full_key = key + ''.join([ch for ch in ALPHABET if ch not in used])
+    
+    matrix = []
+    # Iterate the full key
+    for i in range(0, 25, 5): 
+        row = list(full_key[i:i+5]) 
+        matrix.append(row)  
+
+    return matrix
+
+
+
+
+
