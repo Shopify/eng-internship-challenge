d463498f3536b35858af8afe26527662e2e8ac23
Jasper <halvorsonjasper11@gmail.com>
2024-06-24 13:19:46 -0700

    Matrix with key set up

--- python/solution.py
+++ python/solution.py
@@ -1,14 +1,57 @@
+# Create a matrix with the key first followed by the letters of the alphabet that aren't part of the key
+def createMatrix(key):
+    alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
+    letters = []
 
-def solve(encrypted):
-    # Playfair cypher uses a 5x5 grid of letters
+    # Add in key minus duplicates
+    for ch in key:
+        if ch not in letters:
+            letters.append(ch)
+    # Add rest of alphabet
+    for ch in alphabet:
+        if ch not in key:
+            letters.append(ch)
+
+    # Make into a matrix
+    matrix = []
+    i = 0
+    while i<25:
+        matrix.append(letters[i:i+5])
+        i += 5
+
+    return matrix
+
+def createDigrams(encrypted):
+    digrams = []
+    cur = ""
+    for ch in encrypted:
+        if len(cur) == 2:
+            digrams.append(cur)
+            cur = ""
+        else:
+            cur += ch
+    return digrams
+
+
+        
+
+def solve(encrypted, key):
+    # Playfair cypher uses a 5x5 grid of letters of the alphabet
+    matrix = createMatrix(key)
     # Breaks text into pairs of letters and swaps them in a rectangle within that grid
+    digrams = createDigrams(encrypted)
 
+
+
+    print(digrams)
+    print(matrix)
     return encrypted
 
 
 def main():
     encrypted = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
-    result = solve(encrypted)
+    key = "SUPERSPY"
+    result = solve(encrypted, key)
     print(result)
     return result
 
