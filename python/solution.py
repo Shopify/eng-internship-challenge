73ffdc59189d9f98548aad0af7927b5b2d17a742
Jasper <halvorsonjasper11@gmail.com>
2024-06-24 12:38:28 -0700

    Basic solution structure

---
+++ python/solution.py
@@ -0,0 +1,17 @@
+
+def solve(encrypted):
+    # Playfair cypher uses a 5x5 grid of letters
+    # Breaks text into pairs of letters and swaps them in a rectangle within that grid
+
+    return encrypted
+
+
+def main():
+    encrypted = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV"
+    result = solve(encrypted)
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    main()
