fdaea2289eeaadd0036a1b95d3d0463381530630
Liam Mesrefoglu <qedized@gmail.com>
2024-06-28 18:50:02 -0400

    Added createKeyTable function

--- go/solution.go
+++ go/solution.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// First, we need a function to create the table.
+// Instead of having a 2D array, we can just use a 1D array and calculate the index of the element in the 2D array.
+func createKeyTable(key string) [25]rune {
+	alphabet := "ABCDEFGHIKLMNOPQRSTUVWXYZ" // J is removed
+	key = strings.ToUpper(key)              //makes sure all the characters are uppercase
+	used := make(map[rune]bool)             // To keep track of the used characters
+	keyTable := [25]rune{}                  // The key table
+	tableIndex := 0                         // The index to keep track of the table
+	// Add the characters from the key and the alphabet to the key table
+	for _, c := range key + alphabet {
+		if !used[c] {
+			used[c] = true
+			keyTable[tableIndex] = c
+			tableIndex++
+		}
+	}
+	return keyTable
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	hi := createKeyTable("HELLO")
+	for _, c := range hi {
+		fmt.Printf("%c ", c)
+	}
 }
