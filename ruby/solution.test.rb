42b8dc247299a837f8a87ad3edefb0e182bed4ec
Dropio12 <93799788+Dropio12@users.noreply.github.com>
2024-06-27 20:11:22 -0400

    Update solution.test.rb

--- ruby/solution.test.rb
+++ ruby/solution.test.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 RSpec.describe 'Playfair Decryption Script' do
   it 'outputs the correct decrypted text' do
     output = `ruby solution.rb` # Using backticks to execute the script and capture the output.
