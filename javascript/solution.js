c9a3c2b1a41da9a04865a6495abcfe93eba8af00
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-06-28 20:23:28 -0400

    Refactor solution and remove console log statements

--- javascript/solution.js
+++ javascript/solution.js
@@ -4,9 +4,15 @@ const ENCRYPTED_MESSAGE = "IKEWENENXLNQLPZSLERUMRHEERYBOFNEINCHCV";
 //cipher key
 const CIPHER_KEY = "SUPERSPY";
 
-function decryptMessage(encrypted, key) {
+/**
+ *
+ * @param {Array} cipherKey An cipher keyphrase to use as the basis of a Playfair cipher matrix
+ * @param {Number} squareSize The intended size of the matrix
+ * @returns {Array} An array of squareSize * squareSize dimensions made from the cipher keyphrase given to it
+ */
+function generateCipherMatrix(cipherKey, squareSize) {
   //establish a Set of letters starting with the cipher key,
-  const letterSet = new Set(key.split(""));
+  const letterSet = new Set(cipherKey.split(""));
 
   //add the remaining alphabet letters that have not been used yet, omitting J so that there are 25 letters in total
   const ALPHABET = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
@@ -17,34 +23,66 @@ function decryptMessage(encrypted, key) {
   //turn set into an array to facilitate removing one letter at a time
   const letterArray = Array.from(letterSet);
 
-  //take the cipher key and turn it into a 5x5 matrix
-  const cipherMatrix = [];
+  const matrix = [];
 
-  for (let row = 0; row < 5; row++) {
-    cipherMatrix.push([]);
+  for (let row = 0; row < squareSize; row++) {
+    matrix.push([]);
 
-    for (let col = 0; col < 5; col++) {
-      cipherMatrix[row].push(letterArray.shift());
+    for (let col = 0; col < squareSize; col++) {
+      matrix[row].push(letterArray.shift());
     }
   }
+
+  return matrix;
+}
+
+/**
+ *
+ * @param {Array} matrix A nested array representing a matrix of values
+ * @returns {Array} A nested array where the top level items are now arrays representing the columns of the given matrix rather than the rows
+ */
+function generateColumnsFromMatrix(matrix) {
+  columnSet = [];
+
+  for (let col = 0; col < matrix[0].length; col++) {
+    columnSet.push([]);
+
+    for (let row = 0; row < matrix[0].length; row++) {
+      columnSet[col].push(matrix[row][col]);
+    }
+  }
+  return columnSet;
+}
+
+/**
+ *
+ * @param {String} encryptedMessage A string made of text that was encrypted using the Playfair cipher, which will have an even number of letters if properly created
+ * @returns {Array} An array where each element is a two-letter pair of the encrypted message
+ */
+function generateLetterPairs(encryptedMessage) {
+  const encryptedArray = encryptedMessage.split("");
+  const letterPairs = [];
+  for (let i = 0; i < encryptedArray.length; i += 2) {
+    letterPairs.push([encryptedArray[i], encryptedArray[i + 1]]);
+  }
+  return letterPairs;
+}
+
+/**
+ * 
+ * @param {String} encrypted A set of text encrypted by a Playfair cipher method
+ * @param {String} key The keyphrase meant to be used to decrypt the message
+ * @returns {String} The decrypted message, set to uppercase, where spaces, the letter X, and special characters are removed
+ */
+function decryptMessage(encrypted, key) {
+  //take the cipher key and use it to make a 5x5 matrix
+  const cipherMatrix = generateCipherMatrix(key, 5);
 
   //the cipherMatrix rows are easily accessible, now build a collection of its columns for similar ease of iteration below
-  const cipherColumns = [];
-
-  for (let col = 0; col < 5; col++) {
-    cipherColumns.push([]);
-    for (let row = 0; row < 5; row++) {
-      cipherColumns[col].push(cipherMatrix[row][col]);
-    }
-  }
+  const cipherColumns = generateColumnsFromMatrix(cipherMatrix);
 
   //break the encrypted message into letter pairs
-  const encryptedLetterPairs = [];
-  const encryptedArray = encrypted.split("");
-
-  for (let i = 0; i < encryptedArray.length; i += 2) {
-    encryptedLetterPairs.push([encryptedArray[i], encryptedArray[i + 1]]);
-  }
+  const encryptedLetterPairs = generateLetterPairs(encrypted);
 
   //iterate through letter pairings to check for decrypting conditions
   let decryptedMessage = "";
@@ -55,146 +93,114 @@ function decryptMessage(encrypted, key) {
     let lettersAreInRow;
     let lettersAreInColumn;
 
-    console.log("checking rows");
     //check letter pair to see if they are in the same row of the matrix
     for (let row = 0; row < cipherMatrix.length; row++) {
       const thisRow = cipherMatrix[row];
       lettersAreInRow =
         thisRow.includes(letterPair[0]) && thisRow.includes(letterPair[1]);
 
-      console.log(thisRow, letterPair[0], letterPair[1], lettersAreInRow);
       if (lettersAreInRow) {
         //add decrypted letters to decryptedMessage by
         //getting index of letterPair[0] and letterPair[1] and taking one index to the left, wrapping around as needed
-        const encryptedPair1RowIndex = thisRow.indexOf(letterPair[0]);
-        const encryptedPair2RowIndex = thisRow.indexOf(letterPair[1]);
+        const encryptedLetter1RowIndex = thisRow.indexOf(letterPair[0]);
+        const encryptedLetter2RowIndex = thisRow.indexOf(letterPair[1]);
 
-        const decryptedPair1RowIndex =
-          encryptedPair1RowIndex - 1 >= 0
-            ? encryptedPair1RowIndex - 1
-            : encryptedPair1RowIndex + 4;
+        const decryptedLetter1RowIndex =
+          encryptedLetter1RowIndex - 1 >= 0
+            ? encryptedLetter1RowIndex - 1
+            : encryptedLetter1RowIndex + 4;
 
-        const decryptedPair2RowIndex =
-          encryptedPair2RowIndex - 1 >= 0
-            ? encryptedPair2RowIndex - 1
-            : encryptedPair2RowIndex + 4;
-
-        console.log(
-          thisRow[encryptedPair1RowIndex],
-          thisRow[encryptedPair2RowIndex]
-        );
-        console.log(
-          thisRow[decryptedPair1RowIndex],
-          thisRow[decryptedPair2RowIndex],
-          "\n"
-        );
+        const decryptedLetter2RowIndex =
+          encryptedLetter2RowIndex - 1 >= 0
+            ? encryptedLetter2RowIndex - 1
+            : encryptedLetter2RowIndex + 4;
 
         decryptedMessage +=
-          thisRow[decryptedPair1RowIndex] + thisRow[decryptedPair2RowIndex];
+          thisRow[decryptedLetter1RowIndex] + thisRow[decryptedLetter2RowIndex];
 
         //if the letters have been found in the same row, the rest of the search for columns can be skipped for this pair
         break;
       }
     }
 
-    if (!lettersAreInRow){
-        console.log("checking columns");
+    if (lettersAreInRow) return;
 
-        //if not in the same row, then check letter pair to see if they are in the same column of the matrix
-        for (let col = 0; col < cipherColumns.length; col++) {
-          const thisColumn = cipherColumns[col];
-          lettersAreInCol =
-            thisColumn.includes(letterPair[0]) &&
-            thisColumn.includes(letterPair[1]);
-    
-          console.log(thisColumn, letterPair[0], letterPair[1], lettersAreInCol);
-          if (lettersAreInCol) {
-            //add decrypted letters to decryptedMessage by
-            //getting index of letterPair[0] and letterPair[1] and taking one index to the left, wrapping around as needed
-            const encryptedPair1ColIndex = thisColumn.indexOf(letterPair[0]);
-            const encryptedPair2ColIndex = thisColumn.indexOf(letterPair[1]);
-    
-            const decryptedPair1ColIndex =
-              encryptedPair1ColIndex - 1 >= 0
-                ? encryptedPair1ColIndex - 1
-                : encryptedPair1ColIndex + 4;
-    
-            const decryptedPair2ColIndex =
-              encryptedPair2ColIndex - 1 >= 0
-                ? encryptedPair2ColIndex - 1
-                : encryptedPair2ColIndex + 4;
-    
-            console.log(
-              thisCol[encryptedPair1ColIndex],
-              thisCol[encryptedPair2ColIndex]
-            );
-            console.log(
-              thisCol[decryptedPair1ColIndex],
-              thisCol[decryptedPair2ColIndex],
-              "\n"
-            );
-    
-            decryptedMessage +=
-              thisCol[decryptedPair1ColIndex] + thisCol[decryptedPair2ColIndex];
-    
-            //if the letters have been found in the same columnn, the rest of the search for boxes can be skipped for this pair
-            break;
-          }
-        }
-    
+    //if not in the same row, then check letter pair to see if they are in the same column of the matrix
+    for (let col = 0; col < cipherColumns.length; col++) {
+      const thisColumn = cipherColumns[col];
+      lettersAreInCol =
+        thisColumn.includes(letterPair[0]) &&
+        thisColumn.includes(letterPair[1]);
+
+      if (lettersAreInCol) {
+        //add decrypted letters to decryptedMessage by
+        //getting index of letterPair[0] and letterPair[1] and taking one index higher up the column, wrapping around as needed
+        const encryptedLetter1ColIndex = thisColumn.indexOf(letterPair[0]);
+        const encryptedLetter2ColIndex = thisColumn.indexOf(letterPair[1]);
+
+        const decryptedLetter1ColIndex =
+          encryptedLetter1ColIndex - 1 >= 0
+            ? encryptedLetter1ColIndex - 1
+            : encryptedLetter1ColIndex + 4;
+
+        const decryptedLetter2ColIndex =
+          encryptedLetter2ColIndex - 1 >= 0
+            ? encryptedLetter2ColIndex - 1
+            : encryptedLetter2ColIndex + 4;
+
+        decryptedMessage +=
+          thisCol[decryptedLetter1ColIndex] + thisCol[decryptedLetter2ColIndex];
+
+        //if the letters have been found in the same columnn, the rest of the search for boxes can be skipped for this pair
+        break;
+      }
     }
 
-    //else establish the box between these letters as the corner points and get the other corners as the decrypted letters
-    if (!lettersAreInRow && !lettersAreInColumn){
+    if (lettersAreInColumn) return;
+    //if not in same row or column, establish the box between these letters as the corner points and get the other corners as the decrypted letters
+    
+    //the decrypted letter will exist on the same row as the encrypted letter,
+    //and while the letters may appear in different order in the box,
+    //the letters when decrypted must still be added in the same order in which the encrypted letters were places
 
-        console.log("checking boxes");
-
-        //the decrypted letter will exist on the same row as the encrypted letter,
-        //and while the letters may appear in different order in the box,
-        //the letters when decrypted must still be added in the same order in which the encrypted letters were places
-
-        //check through all the rows to see which row co-ordinate the first letter is at
-        let firstLetterRow;
-        for(firstLetterRow = 0; firstLetterRow < cipherMatrix.length; firstLetterRow++){
-            if (cipherMatrix[firstLetterRow].includes(letterPair[0]))
-                break;
-        }
-        
-        //get column index of first letter
-        let firstLetterCol = cipherMatrix[firstLetterRow].indexOf(letterPair[0]);
-        
-        
-        //check through all the rows to see which row co-ordinate the second letter is at
-        let secondLetterRow;
-        for(secondLetterRow = 0; secondLetterRow < cipherMatrix.length; secondLetterRow++){
-            if (cipherMatrix[secondLetterRow].includes(letterPair[1]))
-                break;
-        }
-        //get column index of second letter
-        let secondLetterCol = cipherMatrix[secondLetterRow].indexOf(letterPair[1]);
-        
-        console.log(letterPair[0], firstLetterRow, firstLetterCol);
-        console.log(letterPair[1], secondLetterRow, secondLetterCol);
-
-        //get the first letter's row, at the column index of the second letter, to decrypt first letter
-        decryptedMessage += cipherMatrix[firstLetterRow][secondLetterCol];
-        
-        //get the second letter's row, at the column index of the first letter, to decrypt second letter
-        decryptedMessage += cipherMatrix[secondLetterRow][firstLetterCol];
-
-
+    //check through all the rows to see which row co-ordinate the first letter is at
+    let firstLetterRow;
+    for (
+      firstLetterRow = 0;
+      firstLetterRow < cipherMatrix.length;
+      firstLetterRow++
+    ) {
+      if (cipherMatrix[firstLetterRow].includes(letterPair[0])) break;
     }
 
+    //get column index of first letter
+    let firstLetterCol = cipherMatrix[firstLetterRow].indexOf(letterPair[0]);
+
+    //check through all the rows to see which row co-ordinate the second letter is at
+    let secondLetterRow;
+    for (
+      secondLetterRow = 0;
+      secondLetterRow < cipherMatrix.length;
+      secondLetterRow++
+    ) {
+      if (cipherMatrix[secondLetterRow].includes(letterPair[1])) break;
+    }
+    //get column index of second letter
+    let secondLetterCol = cipherMatrix[secondLetterRow].indexOf(letterPair[1]);
+
+    //get the first letter's row, at the column index of the second letter, to decrypt first letter
+    decryptedMessage += cipherMatrix[firstLetterRow][secondLetterCol];
+
+    //get the second letter's row, at the column index of the first letter, to decrypt second letter
+    decryptedMessage += cipherMatrix[secondLetterRow][firstLetterCol];
   });
 
-  decryptedMessage += "2!#!   @5/?"
-
-  console.log(decryptedMessage);
   //output must be in upper case, not include spaces, the letter X, or special characters
-  return decryptedMessage.toUpperCase().replaceAll("X","").replace(/[^A-Z ]/g, "");
+  return decryptedMessage
+    .toUpperCase()
+    .replaceAll("X", "")
+    .replace(/[^A-Z]/g, "");
 }
-
-
 
 //decrypted message
 const DECRYPTED_MESSAGE = decryptMessage(ENCRYPTED_MESSAGE, CIPHER_KEY);
